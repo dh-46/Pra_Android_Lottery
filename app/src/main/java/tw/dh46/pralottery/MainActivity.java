17762eb8a9533ecdc1f4658681d5deb1b381f8c3
dh-46 <danielhuang.work@gmail.com>
2018-10-12 19:49:40 +0800

    Self-Practice 1007 Lottery

---
+++ app/src/main/java/tw/dh46/pralottery/MainActivity.java
@@ -0,0 +1,13 @@
+package tw.dh46.pralottery;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
